239dd24553e60f2a90e5385e060efbde9834c66f
brian-mulei <brianmulei0@gmail.com>
2020-11-16 09:36:39 +0300

    new update

--- app/src/main/java/com/hfad/stopwatch/MainActivity.java
+++ app/src/main/java/com/hfad/stopwatch/MainActivity.java
@@ -3,6 +3,7 @@ package com.hfad.stopwatch;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.os.Handler;
 import android.view.View;
 import android.widget.TextView;
 
@@ -15,7 +16,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
+            runTimer();
     }
 
     public void onClickStart(View view){
@@ -34,7 +35,23 @@ public class MainActivity extends AppCompatActivity {
 
 private void runTimer(){
         final TextView timeView= (TextView)findViewById(R.id.time_view);
-        
+        final Handler handler =new Handler();
+        handler.post(new Runnable() {
+            @Override
+            public void run() {
+                int hours =seconds/3600;
+                int minutes =(seconds%3600)/60;
+                int secs =seconds%60;
+                String time =String.format("%d:%02d:%02d",
+                        hours,minutes,secs);
+                timeView.setText(time);
+                if(running){
+                    seconds++;
+                }
+                handler.postDelayed(this,1000);
+            }
+        });
+
 }
 
 
